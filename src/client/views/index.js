fd8ae4c43df0b1de36bacfa78a448ce0c61e660a
Jaspreet Singh <jaspreet_xps@yahoo.com>
2017-02-15 17:48:13 -0800

    Add link create form

--- src/client/views/index.js
+++ src/client/views/index.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import { LinksContainer, LinksRoute } from '../components/links'
+import CreateLinkForm from '../components/createLinkForm'
 import Relay from 'react-relay'
 
 // React Router stuff going on here, looking more like config
 class App extends Component {
   render() {
     return (
-      <Relay.RootContainer
-        Component={LinksContainer}
-        route={new LinksRoute()}
-      />
+      <div>
+        <CreateLinkForm/>
+        <Relay.RootContainer
+          Component={LinksContainer}
+          route={new LinksRoute()}
+        />
+      </div>
     )
   }
 }
